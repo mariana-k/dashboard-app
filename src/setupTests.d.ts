f8494e5c9e84720dae5a8cee8f05405a180bdc27
Mariana Kyrkosh <mariana.kyrkosh@gmail.com>
2025-04-22 20:30:34 +0300

    [ST-18] fixed errors

--- src/setupTests.d.ts
+++ src/setupTests.d.ts
@@ -2,8 +2,8 @@
 
 declare namespace jest {
   interface Matchers<R> {
-    toBeInTheDocument(): R;
-    toHaveClass(className: string): R;
-    toHaveAttribute(attr: string, value?: string): R;
+    toBeInTheDocument(): R
+    toHaveClass(className: string): R
+    toHaveAttribute(attr: string, value?: string): R
   }
 }
