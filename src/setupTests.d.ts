3a2c3245036897419c587201db639019740e486d
Mariana Kyrkosh <mariana.kyrkosh@gmail.com>
2025-04-22 20:25:18 +0300

    [ST-18] fixed types

--- src/setupTests.d.ts
+++ src/setupTests.d.ts
@@ -1,7 +1,9 @@
-import '@testing-library/jest-dom'
+/// <reference types="@testing-library/jest-dom" />
 
-declare global {
-  namespace jest {
-    interface Matchers<R> extends jest.Matchers<R>, Testing.Matchers {}
+declare namespace jest {
+  interface Matchers<R> {
+    toBeInTheDocument(): R;
+    toHaveClass(className: string): R;
+    toHaveAttribute(attr: string, value?: string): R;
   }
 }
